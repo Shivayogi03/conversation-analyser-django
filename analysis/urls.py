25e30089e77c193d01b3f4ddf1d4e9d7b6605a3e
Shivayogi AM <perilious8888@gmail.com>
2025-11-10 23:59:57 +0530

    Import views from analysis in urls.py

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,6 +1,8 @@
 # analysis/urls.py
 from django.urls import path
 from . import views
+from analysis import views
+
 
 urlpatterns = [
     path('conversations/', views.upload_conversation, name='upload_conversation'),
