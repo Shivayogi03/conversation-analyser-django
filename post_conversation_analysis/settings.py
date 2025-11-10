a756bd089f065fcbb36e4b079c98d6b33aa35259
Shivayogi AM <perilious8888@gmail.com>
2025-11-10 23:33:35 +0530

    Allow all hosts in Django settings

--- post_conversation_analysis/settings.py
+++ post_conversation_analysis/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-**!&v6sfakp_0xp7lp7orc&px*xqhlec5bt%c62oy*0jc!j$k)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
