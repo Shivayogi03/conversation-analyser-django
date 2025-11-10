8be6689727ee7d60dd2a0e0c9f1bfc1cfc18c8dc
Shivayogi AM <perilious8888@gmail.com>
2025-11-11 00:07:44 +0530

    Change import from your_app to analysis

--- post_conversation_analysis/urls.py
+++ post_conversation_analysis/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
-from your_app import views
+from analysis import views
 
 
 # ✅ Define the home view
